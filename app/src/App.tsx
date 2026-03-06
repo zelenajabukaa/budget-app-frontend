720c51ab207c95a54dd76e3440ce950c94878070
zelenajabukaa <jakovljevicluka09@gmail.com>
2026-03-06 14:30:29 +0100

    first commit

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { DatePicker } from 'antd'
+
+function App() {
+  return (
+    <DatePicker></DatePicker>
+  )
+}
+
+export default App
