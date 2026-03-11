2a7fe658e76aa9564cad6617ec95645f82f8e8bd
Luka Jakovljevic <luka.jakovljevic@axa.ch>
2026-03-11 14:57:41 +0100

    rearranged the buttons

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,40 +1,41 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import AddButton from './components/buttons/AddButton';
 import Header from './components/header/Header';
 import NoEntries from './components/notifications/NoEntries';
-import { Button, Card } from 'antd';
+import {Button} from 'antd';
 
 function App() {
-  const [isPopupOpen, setIsPopupOpen] = useState(false)
+    const [isPopupOpen, setIsPopupOpen] = useState(false)
 
 
-  return (
-    <>
-      <Header />
-      <AddButton onClick={() => setIsPopupOpen(!isPopupOpen)} />
-      {!isPopupOpen && (
-        <NoEntries message='Einnahmen' />
-      )}
-      {isPopupOpen && (
-        <Card variant={'outlined'}
-          style={{
-            position: 'absolute',
-            width: '40%',
-            height: '7rem',
-            display: 'flex',
-            top: '30%',
-            justifySelf: 'center',
-            justifyContent: 'space-evenly',
-            backgroundColor: '#648ee2',
-            borderColor: '#3a5ea7'
-          }} >
-          <Button style={{ padding: '2rem' }}>Einnahmen Erfassen</Button>
-          <Button style={{ padding: '2rem', marginLeft: '2rem' }}>Ausgaben Erfassen</Button>
-        </Card >
-      )
-      }
-    </>
-  );
+    return (
+        <>
+            <Header/>
+            <AddButton onClick={() => setIsPopupOpen(!isPopupOpen)}/>
+
+            <NoEntries message='Einnahmen'/>
+
+            {!isPopupOpen && (
+                <>
+                    <Button style={{
+                        padding: '2rem',
+                        minWidth: '12rem',
+                        position: 'absolute',
+                        bottom: '8rem',
+                        right: '6rem'
+                    }}>Einnahmen Erfassen</Button>
+
+                    <Button style={{
+                        padding: '2rem',
+                        minWidth: '12rem',
+                        position: 'absolute',
+                        bottom: '14rem',
+                        right: '6rem'
+                    }}>Ausgaben Erfassen</Button>
+                </>
+            )}
+        </>
+    );
 }
 
 export default App
