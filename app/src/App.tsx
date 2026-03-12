011a19b24c4ed023a4b5712435dc63449eabc616
zelenajabukaa <jakovljevicluka09@gmail.com>
2026-03-12 08:23:40 +0100

    trying to make an animation for when you open the small menu

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,9 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import AddButton from './components/buttons/AddButton';
 import Header from './components/header/Header';
 import NoEntries from './components/notifications/NoEntries';
-import {Button} from 'antd';
+import { Button } from 'antd';
+import './App.css'
 
 function App() {
     const [isPopupOpen, setIsPopupOpen] = useState(false)
@@ -10,29 +11,33 @@ function App() {
 
     return (
         <>
-            <Header/>
-            <AddButton onClick={() => setIsPopupOpen(!isPopupOpen)}/>
+            <Header />
+            <AddButton onClick={() => setIsPopupOpen(!isPopupOpen)} />
 
-            <NoEntries message='Einnahmen'/>
+            <NoEntries message='Einnahmen' />
 
             {!isPopupOpen && (
-                <>
-                    <Button style={{
-                        padding: '2rem',
-                        minWidth: '12rem',
-                        position: 'absolute',
-                        bottom: '8rem',
-                        right: '6rem'
-                    }}>Einnahmen Erfassen</Button>
+                <div className={`actionButtons ${isPopupOpen ? 'open' : ''}`}>
+                    <Button
+                        className='action-button'
+                        style={{
+                            padding: '2rem',
+                            minWidth: '12rem',
+                            position: 'absolute',
+                            bottom: '8rem',
+                            right: '6rem'
+                        }}>Einnahmen Erfassen</Button>
 
-                    <Button style={{
-                        padding: '2rem',
-                        minWidth: '12rem',
-                        position: 'absolute',
-                        bottom: '14rem',
-                        right: '6rem'
-                    }}>Ausgaben Erfassen</Button>
-                </>
+                    <Button
+                        className='action-button'
+                        style={{
+                            padding: '2rem',
+                            minWidth: '12rem',
+                            position: 'absolute',
+                            bottom: '14rem',
+                            right: '6rem'
+                        }}>Ausgaben Erfassen</Button>
+                </div>
             )}
         </>
     );
