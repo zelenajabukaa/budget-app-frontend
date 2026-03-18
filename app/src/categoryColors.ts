d53be38b0c87784e3cc15dd0b9ad878176a37754
Luka Jakovljevic <luka.jakovljevic@axa.ch>
2026-03-18 12:50:53 +0100

    made categoryColors a separate file

---
+++ app/src/categoryColors.ts
@@ -0,0 +1,17 @@
+export const categoryColors: { [key: string]: string } = {
+
+    // Earnings
+    'Gehalt': '#3f8600',
+    'Geschenk': '#096dd9',
+    'Verkauf': '#d4b106',
+
+
+    // Expenses
+    'Miete': '#cf1322',
+    'Lebensmittel': '#d46b08',
+    'Transport': '#722ed1',
+    'Freizeit': '#8c8c8c',
+    'Handyrechnung': '#ff9d66',
+
+    'Sonstiges': '#595959',
+};
