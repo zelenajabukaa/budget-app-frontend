84406c502319d3685874652885970eea5a2d749d
Luka Jakovljevic <luka.jakovljevic@axa.ch>
2026-03-25 16:43:03 +0100

    added titles to the earningPage and expensePage

--- app/src/pages/AddExpensePage.tsx
+++ app/src/pages/AddExpensePage.tsx
@@ -1,11 +1,13 @@
 import Header from "../components/header/Header.tsx";
 import AddExpenseForm from "../components/forms/AddExpenseForm.tsx";
+import {Typography} from "antd";
 
 
 function AddExpensePage(){
     return (
         <div>
             <Header/>
+            <Typography.Title style={{color: 'white', justifySelf: 'center'}}>Ausgabe Hinzufügen</Typography.Title>
             <AddExpenseForm/>
         </div>
     )
